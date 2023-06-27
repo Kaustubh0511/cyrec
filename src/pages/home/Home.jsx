326b41f8f80bc735b1361f5e9f7d73cea51a09fe
Kaustubh0511 <kaustubhdayma@gmail.com>
2023-06-27 13:03:55 +0530

    Subdomain Styling

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
+import ReactCardFlip from 'react-card-flip';
+import { images } from '../../constants';
+import News from '../news/News';
 
-export default function Home ()  {
+
+export default function Home() {
+
+  const [flipped, setFlipped] = useState(-1);
+
   return (
-    <div className='text-7xl'>Home</div>
+    <div className="min-h-screen bg-[#090580] py-24 px-10 text-white">
+      <h1 className="font-[algerian] text-[6rem] text-white">CYREC</h1> 
+      <p className="mb-20 font-[helvetica] text-white">Cyber Reconnaissance</p>
+      <div className="flex flex-wrap gap-10 justify-center items-center">
+        {images.map((imgDetails, imgIndex) => (
+          <ReactCardFlip key={imgIndex} isFlipped={imgIndex === flipped} flipDirection='horizontal'>
+            <div className='relative hover:cursor-pointer'>
+              <img className='w-72 h-56 md:w-96 md:h-72 bg-contain bg-center rounded-3xl pointer-events-none' src={imgDetails.img} />
+              <div className='absolute inset-0 bg-gradient-to-b from-transparent to-white rounded-3xl opacity-0 hover:opacity-100'>
+                <p onClick={() => setFlipped(imgIndex)} className='flex justify-center items-end w-full h-full text-black'>{imgDetails.hoverText}</p>
+              </div>
+            </div>
+            <div onClick={() => setFlipped(-1)} className='relative p-3 w-72 h-56 md:w-96 md:h-72 flex flex-col gap-3 justify-center items-center bg-[64CCC5] rounded-3xl hover:cursor-pointer'>
+              <img className='absolute inset-0 w-72 h-56 md:w-96 md:h-72 bg-contain bg-center rounded-3xl pointer-events-none opacity-20 border-4 border-white' src={imgDetails.img} />
+              <h1 className='font-bold sm:text-2xl md:text-1xl lg:text-3xl '>{imgDetails.head}</h1>
+              <p className='font-semibold text-[0.6rem] md:text-[0.8rem] lg:text-[1rem]'>{imgDetails.desc}</p>
+            </div>
+          </ReactCardFlip>
+        ))}
+      </div>
+      {/* <div>
+        <News/>
+      </div> */}
+    </div>
   )
 }
--- src/pages/subdom/Subdom.jsx
+++ src/pages/subdom/Subdom.jsx
@@ -1,63 +1,85 @@
-import React, { useEffect, useState } from 'react'
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
+import React, { useEffect, useState } from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
 
 export default function Emailver() {
-
-  const [ip, setIp] = useState('');
+  const [ip, setIp] = useState("");
   const [info, SetInfo] = useState(null);
 
   const handleChange = (e) => {
     setIp(e.target.value);
-  }
+  };
 
   let API_URL = `https://subdomains.whoisxmlapi.com/api/v1?apiKey=at_mpgHtw2z1PN1o2ZdlrH8wLvcHho3n&domainName=${ip}`;
   const handleClick = async () => {
     await fetch(API_URL)
       .then(async (data) => {
-        let k = await data.json()
+        let k = await data.json();
         console.log(k);
-        SetInfo(k)
+        SetInfo(k);
       })
       .catch((e) => {
         console.log(e);
-      })
-  }
+      });
+  };
 
   return (
     <>
-      <CssBaseline />
-      <Box style={{ marginTop: '10px', padding: '25px' }} sx={{ display: 'inline-flex', bgcolor: '#cfe8fc', width: '600px', height: '250px', justifyContent: 'center' }} > SUBDOMAINS
-        API <br /> With one API call, instantly get a list of all subdomains related to a given domain name to reveal a company’s entire web infrastructure. Use it to identify and fortify potential vulnerabilities. </Box>
+      <Box className="bg-[#000000] text-slate-100">
+        <Box >
+          <h1 class="text-xl font-bold mb-2"> SUBDOMAINS API</h1>
+          <br />{" "}
+          <p>
+            {" "}
+            With one API call, instantly get a list of all subdomains related to
+            a given domain name to reveal a company’s entire web infrastructure.
+            Use it to identify and fortify potential vulnerabilities.{" "}
+          </p>
+        </Box>
 
-
-      <Container maxWidth="xl">
-        <Box style={{ marginTop: '30px' }} sx={{ bgcolor: '#cfe8fc', width: '100%', height: '500px', display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center' }} >
-          <Box sx={{ border: 'solid 2px red', bgcolor: '#cfe8fc', width: '25%', height: '400px', }}>
-            <div>Enter the Domain to check</div>
-            <input type="text" value={ip} onChange={handleChange} />
-            <button onClick={handleClick}>Get Data</button>
+        <Container maxWidth="xl">
+          <Box className="flex justify-center items-center mt-8 mb-3">
+            <Box className="bg-[#e57e17] text-white p-8 rounded-lg shadow-lg w-[50rem]">
+              <div className="font-mono mb-2">Enter the Domain to check</div>
+              <input
+                type="text"
+                value={ip}
+                onChange={handleChange}
+                className="border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 rounded-md px-4 py-2 text-black mb-3"
+              />
+              <button
+                className="ml-3 bg-blue-200 hover:bg-blue-700 hover:text-white text-black font-bold py-2 px-4 rounded"
+                onClick={handleClick}
+              >
+                Get Data
+              </button>
+            </Box>
           </Box>
-          <Box sx={{ border: 'solid 2px red', bgcolor: '#cfe8fc', width: '65%', height: '400px' }}>
+          <Box className="flex-wrap">
             <div>
               <div>Search: {info && info.search}</div>
-              <div>Subdomains Count: {info && info.result &&  info.result.count}</div>
+              <div>
+                Subdomains Count: {info && info.result && info.result.count}
+              </div>
               <ul>
-                {info && info.result && info.result.records && info.result.records.slice(0,10).map((record, index) => (
-                  <li key={index}>
-                    <strong>Domain:</strong> {record.domain}<br />
-                    <strong>First Seen:</strong> {record.firstSeen}<br />
-                    <strong>Last Seen:</strong> {record.lastSeen}
-                  </li>
-                ))}
+                {info &&
+                  info.result &&
+                  info.result.records &&
+                  info.result.records.slice(0, 10).map((record, index) => (
+                    <li key={index}>
+                      <strong>Domain:</strong> {record.domain}
+                      <br />
+                      <strong>First Seen:</strong> {record.firstSeen}
+                      <br />
+                      <strong>Last Seen:</strong> {record.lastSeen}
+                    </li>
+                  ))}
               </ul>
             </div>
-
           </Box>
-        </Box>
-      </Container>
-
+        </Container>
+      </Box>
     </>
-  )
+  );
 }
